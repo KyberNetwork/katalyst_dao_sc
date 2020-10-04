c83b7e92726a7af771f2ba41c9ca6273a32f7e47
Desmond <howeilun@hotmail.com>
2020-10-05 01:41:35 +0800

    Multiple epoch claimer contract (#4)
    * Add multiple epoch rewards claimer, use kyber utils sc

---
+++ contracts/wrappers/MultipleEpochRewardsClaimer.sol
@@ -0,0 +1,53 @@
+pragma solidity 0.6.6;
+
+import "../IKyberDao.sol";
+import "./IMultipleEpochRewardsClaimer.sol";
+import "@kyber.network/utils-sc/contracts/Withdrawable.sol";
+
+
+contract MultipleEpochRewardsClaimer is IMultipleEpochRewardsClaimer, Withdrawable {
+    IKyberDao public immutable kyberDao;
+
+    constructor(IKyberDao _kyberDao, address _admin) public Withdrawable(_admin) {
+        kyberDao = _kyberDao;
+    }
+
+    /// @dev unclaimedEpochs is asusumed to be of reasonable length
+    /// otherwise txns might run out of gas
+    function claimMultipleRewards(
+        IFeeHandler feeHandler,
+        uint256[] calldata unclaimedEpochs
+    ) external override {
+        for (uint256 i = 0; i < unclaimedEpochs.length; i++) {
+            feeHandler.claimStakerReward(msg.sender, unclaimedEpochs[i]);
+        }
+    }
+
+    function getUnclaimedEpochs(IFeeHandler feeHandler, address staker)
+        external
+        view
+        override
+        returns (uint256[] memory unclaimedEpochs)
+    {
+        uint256 currentEpoch = kyberDao.getCurrentEpochNumber();
+        uint256[] memory tempArray = new uint256[](currentEpoch);
+        uint256 i;
+        uint256 j;
+        // full array size is expected to be of reasonable length
+        // for the next 1-2 years
+        // we thus start iterating from epoch 0
+        for (i = 0; i < currentEpoch; i++) {
+            if (
+                !feeHandler.hasClaimedReward(staker, i) &&
+                kyberDao.getPastEpochRewardPercentageInPrecision(staker, i) != 0
+            ) {
+                tempArray[j]= i;
+                j++;
+            }
+        }
+        unclaimedEpochs = new uint256[](j);
+        for (i = 0; i < j; i++) {
+            unclaimedEpochs[i] = tempArray[i];
+        }
+    }
+}
